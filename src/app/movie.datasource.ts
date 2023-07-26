f52f47d074a13b3a9e694b75fdb9e9f0992a8d5c
ebrardev <16155052@mersin.edu.tr>
2023-07-26 20:40:47 +0300

    smthng error dashboard

--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -1,8 +1,9 @@
 import {Movie}  from './movie';
 
 export const Movies: Movie[] = [
-    { title: "The Shawshank Redemption", year: 1994 },  
-    { title: "The Godfather", year: 1972 },
-    { title: "The Godfather: Part II", year: 1974 },
-    { title: "The Dark Knight", year: 2008 },
+    { title: "The Shawshank Redemption", year: 1994 , description:"hapishane", imageUrl:"1.jpeg"},  
+    { title: "The Godfather", year: 1972, description:"baba", imageUrl:"2.jpeg" },
+    { title: "The Godfather: Part II", year: 1974 , description:"baba 2", imageUrl:"3.jpeg"},
+    { title: "The Dark Knight", year: 2008, description:"batmanlı", imageUrl:"4.jpeg" },
+    {title:"fight club " , year:1999 , description:"dövüş kulübü", imageUrl:"5.jpeg"},
 ];
--- src/app/movie.ts
+++ src/app/movie.ts
@@ -1,9 +1,13 @@
 export class Movie {
     title: string;
     year: number;
+    description: string;
+    imageUrl: string;
 
     constructor(title: string, year: number) {
         this.title = title;
         this.year = year;
+        this.description = '';
+        this.imageUrl = '';
     }
 }
