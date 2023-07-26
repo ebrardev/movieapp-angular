67c64ec715c7a8a2ac8020cc29ccd96f053290b9
ebrardev <16155052@mersin.edu.tr>
2023-07-26 19:16:25 +0300

    logging component

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -3,6 +3,8 @@ import { Movie } from './movie';
 import { Movies } from './movie.datasource';
 import { Observable, of } from 'rxjs';
 
+import { LoggingService } from './logging.service';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,9 +12,10 @@ export class MovieService {
 
   
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) {  }
 
   getMovies(): Observable< Movie[]> {
+    this.loggingService.add('MovieService: fetched movies');
     return of(Movies);
   }
 
