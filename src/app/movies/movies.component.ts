d9fd7b66e5c4c7e45ba9fdb90bfffeb0aab3137a
ebrardev <16155052@mersin.edu.tr>
2023-07-26 14:49:28 +0300

    ng g s service

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,18 +1,28 @@
-import { Component } from "@angular/core";
-import { Movies } from "../movie.datasource";
+import { Component, OnInit } from "@angular/core";
 import { Movie } from "../movie";
+import { MovieService } from "../movie.service";
 
 @Component({
   selector: 'movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit {
   title = "List of Movies";
-  movies = Movies;
-  selectedMovie: Movie | null = null; // Başlangıçta null değeri atanmış
+  movies: Movie[] = [];
+  selectedMovie: Movie | null = null; 
+
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit(): void {
+    this.getMovies(); 
+  }
 
   onSelect(movie: Movie): void {
     this.selectedMovie = movie;
+  }
+  
+  getMovies(): void {
+    this.movies = this.movieService.getMovies();
   }
 }
