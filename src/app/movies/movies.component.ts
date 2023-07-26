f311a337e803f755f9edc0be276b18fbb9917f7c
ebrardev <16155052@mersin.edu.tr>
2023-07-26 13:59:38 +0300

    events

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,14 +1,23 @@
 import { Component } from "@angular/core";
-import { Movies} from "../movie.datasource";
+import { Movies } from "../movie.datasource";
+import { Movie } from "../movie";
 
 @Component({
-    selector: 'movies',
+  selector: 'movies',
   templateUrl: './movies.component.html',
-    styleUrls: ['./movies.component.css']
+  styleUrls: ['./movies.component.css']
 })
 
 export class MoviesComponent {
-    title = "List of Movies";
-    movies = Movies;
+  title = "List of Movies";
+  movies = Movies;
+  selectedMovie: Movie | null = null;; // Değişkeni başlangıçta tanımla
 
+  constructor() {
+    this.selectedMovie = this.movies[0]; // Yapıcı fonksiyonda değer atama
+  }
+
+  onSelect(movie: Movie): void {
+    this.selectedMovie = movie;
+  }
 }
