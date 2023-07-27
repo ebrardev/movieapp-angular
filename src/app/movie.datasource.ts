b1b680706feea447ae6716079471116b668cdbfb
ebrardev <16155052@mersin.edu.tr>
2023-07-27 11:07:15 +0300

    dash component

--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -6,4 +6,8 @@ export const Movies: Movie[] = [
     { title: "The Godfather: Part II", year: 1974 , description:"baba 2", imageUrl:"3.jpeg"},
     { title: "The Dark Knight", year: 2008, description:"batmanlı", imageUrl:"4.jpeg" },
     {title:"fight club " , year:1999 , description:"dövüş kulübü", imageUrl:"5.jpeg"},
+    {title:"pulp fiction" , year:1994 , description:"pulp fiction", imageUrl:"6.jpeg"},
+    {title:"forrest gump" , year:1994 , description:"forrest gump", imageUrl:"7.jpeg"},
+    {title:"inception" , year:2010 , description:"inception", imageUrl:"8.jpeg"},
+    {title:"the matrix" , year:1999 , description:"the matrix", imageUrl:"9.jpeg"},
 ];
